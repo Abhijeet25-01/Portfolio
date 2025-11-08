e03b301bbf53f0ca94e32c1c1938c05e0b62f8b3
Abhijeet Singh <abhijeetalok.singh@gmail.com>
2025-11-09 01:12:26 +0530

    changes

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 // Create the Angular module
 var app = angular.module('portfolioApp', ['ngRoute']);
 
-// Configure the routes
+// Configure routes
 app.config(function($routeProvider) {
-    $routeProvider
-        .when('/', {
-            templateUrl: 'pages/home.html'
-        })
-        .when('/about', {
-            templateUrl: 'pages/about.html'
-        })
-        .otherwise({
-            redirectTo: '/'
-        });
+  $routeProvider
+    .when('/', {
+      templateUrl: 'pages/home.html'
+    })
+    .when('/about', {
+      templateUrl: 'pages/about.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 });
